c2a53573ef4a410dbdf9ea7aa3b8e21edb67e7ed
Crysknife <ad.dolorem@yandex.ru>
2017-03-13 08:28:41 +0300

    Reincarnate

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import reducerInstruments from './reducer-instruments';
-
-const allReducers = combineReducers({
-	instruments: reducerInstruments
-});
-
-export default allReducers;
